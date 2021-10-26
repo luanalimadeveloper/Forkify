4a2084717746b5eb792d725d109cdd8de8283e89
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-25 22:16:59 -0300

    Event Handlers in MVC Publisher Subscriber Pattern

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,9 +1,10 @@
-import { assync } from 'regenerator-runtime';
-import { TIMEOUT_SEC } from './helpers.js';
+//import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config.js';
 
+let timeoutID;
 const timeout = function (s) {
   return new Promise(function (_, reject) {
-    setTimeout(function () {
+    timeoutID = setTimeout(function () {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
@@ -11,7 +12,8 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([(fetch(url), timeout(TIMEOUT_SEC))]);
+    const res = await Promise.race([fetch(url), timeout(10)]);
+    clearTimeout(timeoutID);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
