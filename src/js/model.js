fb16b5789c73c7ddae80a5b787c2b37f35ace7c8
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-26 16:28:00 -0300

    Implementing Search Results - Part 1.3

--- src/js/model.js
+++ src/js/model.js
@@ -49,10 +49,8 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results);
   } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
   }
 };
-loadSearchResults('pizza');
