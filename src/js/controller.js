6a6f93222a48e2eb3c4ab4272746c27219066d94
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-29 22:37:58 -0300

    Implementing Pagination - Part 2.1

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -39,22 +40,29 @@ const controlSearchResults = async function () {
 
     // 1) Get search query
     const query = searchView.getQuery();
-
     if (!query) return;
 
     // 2) Load search results
     await model.loadSearchResults(query);
 
     // 3) Render results
+    //resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(6));
 
-    resultsView.render(model.state.search.results);
+    //4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function () {
+  console.log('Pag controller');
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
